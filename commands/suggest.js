3ff3a1f3a2f45fa9fb20d86565bb9f6c3cf3d17b
ItzLokiHere <66423469+ItzLokiHere@users.noreply.github.com>
2021-01-09 16:46:06 +0100

    updatio for suggesto

--- commands/suggest.js
+++ commands/suggest.js
@@ -6,22 +6,21 @@ module.exports = {
 	execute(message, args) {
         const Discord = require('discord.js')
         const bot = new Discord.Client()
-        let suggestChannel = bot.channels.cache.get('797486928899473466')
         let msgArgs = args.slice(0).join(" ")
         
         let embeds = new Discord.MessageEmbed()
-        .setTimestamp("Suggestion")
+        .setTitle("Suggestion")
         .setDescription(msgArgs)
         .setFooter(`Created by ${message.author.tag}. Vote now!`)
         .setTimestamp()
         .setColor("BLACK")
 
-        suggestChannel.send(embeds).then(embeds => {
-            embeds.react(":arrow_up:")
-            embeds.react(":arrow_down:")
+        message.guild.channels.cache.get('797486928899473466').send(embeds).then(embeds => {
+            embeds.react("⬆️")
+            embeds.react("⬇️")
         })
 
-        message.channels.send("Your suggestion has been submited in #suggestions. Thanks!")
+        message.author.send("Your suggestion has been submited in #suggestions. Thanks!")
        
 	}
-};
+};  
